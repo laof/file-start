a64fb9bd4dcd14d3edd35ca03016587ca4b82880
lovalong <lovalong@mfcgd.com>
2020-12-15 13:50:36 +0800

    update

--- app/config.js
+++ app/config.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const homPage = "/ui.html";
 const sharedPath = path.join(process.cwd());
-const IP = require("./app/tools/address");
-const port = require("./app/tools/port");
+const IP = require("./tools/address");
+const port = require("./tools/port");
 
 module.exports = {
   homPage: homPage,
