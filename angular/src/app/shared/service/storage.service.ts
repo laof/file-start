098a65a544221421aff7704b029702b642061cc9
lovalong <lovalong@mfcgd.com>
2020-12-23 12:29:33 +0800

    update

--- angular/src/app/shared/service/storage.service.ts
+++ angular/src/app/shared/service/storage.service.ts
@@ -1,78 +1,71 @@
-import { Injectable } from "@angular/core";
-import { Interface } from "readline";
-import { LocalStorage } from "./class.storage";
-
+import { Injectable } from '@angular/core';
+import { Interface } from 'readline';
+import { LocalStorage } from './class.storage';
 
 @Injectable({ providedIn: 'root' })
 export class PathService extends LocalStorage {
-    constructor() {
-        super('path')
-    }
+  constructor() {
+    super('path');
+  }
 }
 
 @Injectable({ providedIn: 'root' })
 export class SocketIDService extends LocalStorage {
-    constructor() {
-        super('socket-id')
-    }
+  constructor() {
+    super('socket-id');
+  }
 }
 
 @Injectable({ providedIn: 'root' })
 export class ViewModeService extends LocalStorage {
-    constructor() {
-        super('view-mode')
-    }
+  constructor() {
+    super('view-mode');
+  }
 }
 
 @Injectable({ providedIn: 'root' })
 export class GridLayoutService extends LocalStorage {
-    constructor() {
-        super('grid-layout')
-    }
+  constructor() {
+    super('grid-layout');
+  }
 }
 
 @Injectable({ providedIn: 'root' })
 export class LastViewService extends LocalStorage {
-    constructor() {
-        super('last-view')
-    }
+  constructor() {
+    super('last-view');
+  }
 }
 
-
 export interface ViewHistory {
-    date: string;
-    time: string;
-    fileName: string;
-    path: string;
+  date: string;
+  time: string;
+  fileName: string;
+  path: string;
 }
 
 @Injectable({ providedIn: 'root' })
 export class ViewHistoryService extends LocalStorage {
-    private max = 20;
-    constructor() {
-        super('view-history')
+  private max = 20;
+  constructor() {
+    super('view-history');
+  }
+
+  setList(item: ViewHistory) {
+    const list = this.getList();
+    if (list.length >= this.max) {
+      list.pop();
     }
+    list.unshift(item);
+    this.setItem(JSON.stringify(list));
+  }
 
-    setList(item: ViewHistory) {
-        const list = this.getList();
-        list.push(item);
-        if (list.length > this.max) {
-            list.pop();
-        }
-        this.setItem(JSON.stringify(list));
+  getList(): ViewHistory[] {
+    try {
+      const his = this.getItem();
+      return his ? JSON.parse(his) : [];
+    } catch (e) {
+      return [];
     }
-
-    getList(): ViewHistory[] {
-
-        try {
-            const his = this.getItem()
-            return his ? JSON.parse(his).reverse() : [];
-        } catch (e) {
-            return [];
-        }
-
-    }
-
+  }
 }
-
-
